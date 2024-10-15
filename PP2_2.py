e37d6421a5ed93703de741f159f690e84d91617e
Stephen-Li-ICD2O1-01 <stephen.li@student.tdsb.on.ca>
2024-10-15 14:29:47 +0000

    commit

--- PP2_2.py
+++ PP2_2.py
@@ -1,17 +1,32 @@
 
 
 def q1(): 
-  #Write Assignment code here
-
+  num = int(input("Input an integer: "))
+  if num == 5:
+    print("The number is Five")
+  else:
+    print("The number is not Five")
 
 def q2(): 
-  #Write Assignment code here
+  num = int(input("Input a number: "))
+  if num > 0:
+    print("Positive")
+  else:
+    print("Negative")
 
 def q3(): 
-  #Write Assignment code here
+  num = int(input("Input an integer: "))
+  if num % 2 == 0:
+    print("Even")
+  else:
+    print("Odd")
 
 def q4(): 
-  #Write Assignment code here
+  word = str(input("Type \"Hello\": "))
+  if word == ("Hello"):
+    print("The word is Hello")
+  else:
+    print("The word is not Hello")
 
 
 
